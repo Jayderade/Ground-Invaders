4daa70d1eb9e1b459cfec7d54be547debb777212
Jayderade <jcastleden@gmail.com>
2017-09-05 12:05:49 +1000

    more work

---
+++ Assets/EnemyGun.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGun : MonoBehaviour {
+
+    public GameObject enemybullet;
+    // Speed at which the projectile will be flung
+    public float projectileSpeed = 20f;
+    // Rate of fire
+    public float shootRate = .1f;
+
+    // Timer to count up to shootRate
+    private float shootTimer = 0f;
+    private int random;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        shootTimer += Time.deltaTime;
+
+        random = Random.Range(1, 27);
+
+
+        if (shootTimer >= shootRate && random == 1)
+        {
+            // CALL Shoot()
+            Fire();
+            // RESET shootTimer = 0
+            shootTimer = 0;
+        }
+    }
+
+    void Fire()
+    {
+        // Instantiate a new copy of projectilePrefab
+        GameObject projectile = Instantiate(enemybullet);
+        // Position of projectile = transform position
+        projectile.transform.position = transform.position;
+        // Apply a force to the projectile
+        Rigidbody2D projectileRigid = projectile.GetComponent<Rigidbody2D>();
+        projectileRigid.AddForce(transform.up * projectileSpeed, ForceMode2D.Impulse);
+    }
+
+}
--- Assets/Spawn.cs
+++ Assets/Spawn.cs
@@ -4,33 +4,74 @@ using UnityEngine;
 
 public class Spawn : MonoBehaviour {
 
-    public GameObject enemy;
+    public GameObject[] spawnPrefabs;
     public float spawnRadius = 1f;
-    public float spawnRate = 1f;
-   
+    public int spawnAmount = 1;
 
-    private void OnDrawGizmos()
+    public int currentSpawn;
+    public int maxSpawn = 10;
+    public int spawnInterval = 1;
+    private bool hasBalled = false;
+
+    void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, spawnRadius);
     }
 
-    void SpawnEnemy()
-    {
-        // Instantiate a new GameObject
-        GameObject enemySpawned = Instantiate(enemy);
-        //Position it to a random place within the screen
-        enemy.transform.position = Random.insideUnitCircle * spawnRadius;
-
-    }
-
     // Use this for initialization
-    void Start () {
-        Invoke("SpawnEnemy", spawnRate);
+    void Start()
+    {
+        currentSpawn = 0;
+
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasBalled && currentSpawn < maxSpawn)
+        {
+
+            StartCoroutine(Fire());
+            currentSpawn++;
+        }
+    }
+
+    IEnumerator Fire()
+    {
+        // run whatever is here first
+        hasBalled = true;
+
+        // Spawn the bullet
+        SpawnBalls();
+
+
+        yield return new WaitForSeconds(spawnInterval); // wait a few seconds
+
+        // run whatever is here last
+        hasBalled = false;
+    }
+
+    void SpawnBalls()
+    {
+        for (int i = 0; i < spawnAmount; i++)
+        {
+            // Spawned new GameObject
+            int randomIndex = Random.Range(0, spawnPrefabs.Length);
+
+            // Store randomly selected prefab
+            GameObject randomPrefab = spawnPrefabs[randomIndex];
+
+            // Instantiate randomly selected prefab
+            GameObject clone = Instantiate(randomPrefab);
+
+            // Calculate random position within sphere
+            float x = 0; // 
+            float y = 0;
+            float z = 0; // 
+            Vector3 randomPos = transform.position + new Vector3(x, y, z); // calculate random position
+
+            // Set spawned object's position
+            clone.transform.position = randomPos;
+        }
+    }   
 }
